75240472b8c1cb90ead877dc5791b1c225ccec10
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-04-20 11:09:51 -0400

    added multipleEvents

--- DEMOS/demo_multipleEventListeners/multipleEvent.js
+++ DEMOS/demo_multipleEventListeners/multipleEvent.js
@@ -32,36 +32,47 @@
 
 // })
 
-//OR you can use event listener (below)
+//OR you can use event delegation (below)
 
-let removeButton = document.querySelectorAll('li button');
 let addButton = document.querySelector('#add-friend');
 let input = document.querySelector('#first-name');
-let friendList = document.querySelector('#friend-list')
+let friendList = document.querySelector('#friend-list');
+
+let bestFriendButton = document.querySelector('#add-best-friend');
+let inputBestFriend = document.querySelector('#best-friend');
+
+friendList.addEventListener('click', function(event){
+    console.log(event.target);
+    if(event.target.tagName === 'BUTTON'){
+        event.target.parentElement.remove();
+    }else if(event.target.tagName === 'LI'){
+        console.log('you click an li');
+    }
+})
 
 addButton.addEventListener('submit', function(event){
     event.preventDefault();
     let newFriend = document.createElement('li');
     let removeBtn = document.createElement('button');
-
-    friendList.addEventListener('click', function(event){
-        console.log(event.target);
-        if(event.target.tagName === 'button'){
-            event.target.parentElement.remove();
-        }
-    })
-
-    console.log(input.value);
     
     newFriend.innerText = input.value;
-    input.value = "";
-    friendList.append(newFriend);
-    
     removeBtn.innerText = "Delete Friend"
+    friendList.append(newFriend);
     newFriend.append(removeBtn);
-    removeBtn.addEventListener('click', function(event){
-        event.target.parentElement.remove();
-    })
+})
 
+bestFriendButton.addEventListener('submit', function(event){
+    event.preventDefault();
+    let newBestFriend = document.createElement('li');
+    let removeBtn = document.createElement('button');
+    let heart = document.createElement('span');
 
+    removeBtn.innerText = "Delete Friend"
+    newBestFriend.innerText = inputBestFriend.value;
+    newBestFriend.classList.add('best-friend');
+    heart.innerHTML = '&hearts;';
+
+    friendList.append(newBestFriend);
+    newBestFriend.append(removeBtn);
+    newBestFriend.prepend(heart);
 })
